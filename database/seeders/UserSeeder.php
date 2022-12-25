18709505a75ac3e4b43a233909404d63f3d52d3a
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2022-12-25 23:22:14 +0700

    Revert "update"
    This reverts commit ccda2b9b6c86d98571ba5de982f0298601443ff5.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,24 +14,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // User::find(1)->assignRole('Superadmin');
-        // User::find(2)->assignRole('Puskesmas');
-        // User::find(3)->assignRole('Puskesmas');
-        // User::find(4)->assignRole('Puskesmas');
-        // User::find(5)->assignRole('Puskesmas');
-        // User::find(6)->assignRole('Puskesmas');
-        // User::find(7)->assignRole('Puskesmas');
-        // User::find(8)->assignRole('Puskesmas');
-        // User::find(9)->assignRole('Puskesmas');
-        // User::find(10)->assignRole('Puskesmas');
-        // User::find(11)->assignRole('Puskesmas');
-        // User::find(12)->assignRole('Puskesmas');
-        // User::find(13)->assignRole('Puskesmas');
-        // User::find(14)->assignRole('Puskesmas');
-        // User::find(15)->assignRole('Puskesmas');
-        // User::find(16)->assignRole('Puskesmas');
-        // User::find(17)->assignRole('Puskesmas');
-        // User::find(19)->assignRole('Superadmin');
+        User::find(1)->assignRole('Superadmin');
+        User::find(2)->assignRole('Puskesmas');
+        User::find(3)->assignRole('Puskesmas');
+        User::find(4)->assignRole('Puskesmas');
+        User::find(5)->assignRole('Puskesmas');
+        User::find(6)->assignRole('Puskesmas');
+        User::find(7)->assignRole('Puskesmas');
+        User::find(8)->assignRole('Puskesmas');
+        User::find(9)->assignRole('Puskesmas');
+        User::find(10)->assignRole('Puskesmas');
+        User::find(11)->assignRole('Puskesmas');
+        User::find(12)->assignRole('Puskesmas');
+        User::find(13)->assignRole('Puskesmas');
+        User::find(14)->assignRole('Puskesmas');
+        User::find(15)->assignRole('Puskesmas');
+        User::find(16)->assignRole('Puskesmas');
+        User::find(17)->assignRole('Puskesmas');
+        User::find(19)->assignRole('Superadmin');
         // User::create(
         //     [
         //         'name' => 'PUSKESMAS BATANGTORU',
@@ -52,55 +52,55 @@ class UserSeeder extends Seeder
         //     ]
         // )->assignRole('Puskesmas');
 
-        User::create(
-            [
-                'name' => 'PUSKESMAS TES',
-                'email' => 'puskesmastes@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'is_active' => 1,
-            ]
-        )->assignRole('Puskesmas');
+        // User::create(
+        //     [
+        //         'name' => 'PUSKESMAS TES',
+        //         'email' => 'puskesmastes@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('password'),
+        //         'is_active' => 1,
+        //     ]
+        // )->assignRole('Puskesmas');
 
 
-        User::create(
-            [
-                'name' => 'PENINJAU',
-                'email' => 'peninjau@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'is_active' => 1,
-            ]
-        )->assignRole('Peninjau');
+        // User::create(
+        //     [
+        //         'name' => 'PENINJAU',
+        //         'email' => 'peninjau@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('password'),
+        //         'is_active' => 1,
+        //     ]
+        // )->assignRole('Peninjau');
 
-        User::create(
-            [
-                'name' => 'Administrator',
-                'email' => 'administrator@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'is_active' => 1,
-            ]
-        )->assignRole('Administrator');
+        // User::create(
+        //     [
+        //         'name' => 'Administrator',
+        //         'email' => 'administrator@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('password'),
+        //         'is_active' => 1,
+        //     ]
+        // )->assignRole('Administrator');
 
-        User::create(
-            [
-                'name' => 'DINAS KESEHATAN DAERAH SIDIMPUAN',
-                'email' => 'dinkespsp@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'is_active' => 1,
-            ]
-        )->assignRole('Superadmin');
+        // User::create(
+        //     [
+        //         'name' => 'DINAS KESEHATAN DAERAH TAPSEL',
+        //         'email' => 'dinkestapsel@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('password'),
+        //         'is_active' => 1,
+        //     ]
+        // )->assignRole('Superadmin');
 
-        User::create(
-            [
-                'name' => 'YOVIANSYAH RIZKI PRATAMA',
-                'email' => 'yoviansyahrizkypratama@gmail.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'is_active' => 1,
-            ]
-        )->assignRole('Superadmin');
+        // User::create(
+        //     [
+        //         'name' => 'YOVIANSYAH RIZKI PRATAMA',
+        //         'email' => 'yoviansyahrizkypratama@gmail.com',
+        //         'email_verified_at' => now(),
+        //         'password' => bcrypt('password'),
+        //         'is_active' => 1,
+        //     ]
+        // )->assignRole('Superadmin');
     }
 }
